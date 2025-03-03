cef15334cc9fec3355ffda06f716cf73f542bad2
samanasiva28 <samanasiva2@gmail.com>
2025-03-03 16:13:30 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.region  # This is still the region, like 'us-central1'
+
+  deletion_protection = false
+
+  initial_node_count = var.node_count
+
+  node_config {
+    machine_type = var.node_machine_type
+    disk_size_gb = 30
+    # Adding zone here for node pool creation
+  }
+
+  remove_default_node_pool = false
+}
