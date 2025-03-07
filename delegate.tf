b3af928564256c4bd87c5e1ab60ada3518d2ff03
sushma-sm <gurajasushmamounika@gmail.com>
2025-03-07 15:08:50 +0530

    Create delegate.tf

---
+++ delegate.tf
@@ -0,0 +1,41 @@
+# Fetch GKE Cluster Details
+data "google_client_config" "default" {}
+
+data "google_container_cluster" "gke_cluster" {
+  name     = var.cluster_name
+  location = var.region # Change as per your cluster
+  depends_on = [google_container_cluster.primary]
+}
+
+# Kubernetes Provider using GKE authentication
+provider "kubernetes" {
+  host                   = "https://${data.google_container_cluster.gke_cluster.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_container_cluster.gke_cluster.master_auth[0].cluster_ca_certificate)
+}
+
+# Helm Provider using GKE authentication
+provider "helm" {
+  kubernetes {
+    host                   = "https://${data.google_container_cluster.gke_cluster.endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(data.google_container_cluster.gke_cluster.master_auth[0].cluster_ca_certificate)
+  }
+}
+
+
+module "delegate" {
+  source = "harness/harness-delegate/kubernetes"
+  version = "0.1.8"
+
+  account_id = "ucHySz2jQKKWQweZdXyCog"
+  delegate_token = "NTRhYTY0Mjg3NThkNjBiNjMzNzhjOGQyNjEwOTQyZjY="
+  delegate_name = "terraform-delegate"
+  deploy_mode = "KUBERNETES"
+  namespace = "harness-delegate-ng"
+  manager_endpoint = "https://app.harness.io"
+  delegate_image = "harness/delegate:25.02.85300"
+  replicas = 1
+  upgrader_enabled = true
+  depends_on       = [google_container_cluster.primary]
+}
